f4017bbe65de4b562ba65f6d153395989601776b
Natalie Howley <nataliehowley@outlook.com>
2023-06-29 13:35:12 +0200

    add: es-lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, ConfigProvider } from 'antd'
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary: '#CF2D3F'
+      }
+    }}
+  >
+    <Button type='primary'>Click me</Button>
+  </ConfigProvider>
+)
 
-export default App;
+export default App
