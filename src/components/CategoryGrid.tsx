90c02bd8c92a2a7ba5a8c5b5f22793a2979d9410
Natalie Howley <nataliehowley@outlook.com>
2023-06-29 20:25:56 +0200

    feat: add card to category grid

--- src/components/CategoryGrid.tsx
+++ src/components/CategoryGrid.tsx
@@ -1,5 +1,5 @@
 import type { FC } from 'react'
-import { theme } from 'antd'
+import { List, Card, Typography } from 'antd'
 
 interface Props {
   categories: any[]
@@ -8,15 +8,28 @@ interface Props {
 export const CategoryGrid: FC<Props> = ({
   categories
 }) => {
-  const {
-    token: { colorBgContainer }
-  } = theme.useToken()
-
   return (
-      <div className="site-layout-content" style={{ background: colorBgContainer }}>
-        {categories.map(({ name }, index) => (
-          <p key={index}>{name}</p>
-        ))}
-      </div>
+    <List
+    grid={{
+      gutter: 16,
+      xs: 1,
+      sm: 2,
+      md: 4,
+      lg: 4,
+      xl: 6,
+      xxl: 3
+    }}
+    dataSource={categories}
+    renderItem={({ name, iconUrl }) => (
+      <List.Item>
+        <Card>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+            <img alt={name} src={iconUrl} width={50} height={50}/>
+            <Typography style={{ marginTop: '10px', fontWeight: 'bold' }}>{name}</Typography>
+          </div>
+        </Card>
+      </List.Item>
+    )}
+  />
   )
 }
