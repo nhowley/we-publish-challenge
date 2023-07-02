9d47f754b60abf7339de54b3b94eaddfc648d7e8
Natalie Howley <nataliehowley@outlook.com>
2023-07-02 19:24:56 +0200

    fix: styles

--- src/pages/CategoriesPage.styled.tsx
+++ src/pages/CategoriesPage.styled.tsx
@@ -2,6 +2,6 @@ import { Footer } from 'antd/es/layout/layout'
 import styled from 'styled-components'
 
 export const StyledFooter = styled(Footer)`
-   text-align: 'center'; 
-   margin-top: 'auto';
+   text-align: center; 
+   margin-top: auto;
 `
