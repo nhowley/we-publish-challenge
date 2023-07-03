3e4e6aeff464189a4cd9ae4f863f4716424cc53f
Natalie Howley <nataliehowley@outlook.com>
2023-07-03 10:30:57 +0200

    chore: remove unused code

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,9 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra')
+const { override, fixBabelImports } = require('customize-cra')
 
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
     style: true
-  }),
-  addLessLoader({
-    javascriptEnabled: true,
-    modifyVars: {
-      '@primary-color': '#CF2D3F' // customize as needed
-    }
   })
 )
